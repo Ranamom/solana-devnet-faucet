f3366204f0b9b9415102f8db7b9d39af4e1bf828
Mohamed Monir Ali AL Keshita <99746359+Ranamom@users.noreply.github.com>
2023-08-20 20:22:09 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -191,7 +191,7 @@ export default function Home() {
                 <div className="w-full relative flex items-center justify-center p-5 py-10">
                   <Skeleton className="absolute w-[298px] h-[62px]" />
                   <Turnstile
-                    sitekey="0x4AAAAAAAJBWed_emSm9uaO"
+                    sitekey="0x4AAAAAAAJBWQ4bBlSVSV4TyKEoqaqHe90"
                     onVerify={(token) => {
                       requestAirdrop(token);
                     }}
